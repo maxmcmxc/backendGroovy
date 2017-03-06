41deb42c7454a106141c4ba1c3312ba6d2065977
max <maxence.fromentin@hei.fr>
2017-03-06 11:13:17 -0500

    Adding POSTGRESQL Docker connexion, adding two routes to create database and select all.

--- src/main/groovy/spark/gradle/MainGroovy.groovy
+++ src/main/groovy/spark/gradle/MainGroovy.groovy
@@ -1,12 +1,27 @@
 package spark.gradle
 
 import static spark.Spark.*
-
 import groovy.json.JsonBuilder
+import groovy.sql.Sql
+
+import java.security.Provider.Service
+import java.sql.Connection
+import java.sql.Statement
+import java.sql.DriverManager
 
 service = new UserService()
+dbTest = new DBtest()
 
 port(7777)
+
+//webSocket("/echo", EchoWeb.class);
+//init(); // Needed if you don't define any HTTP routes after your WebSocket routes
+
+//Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:5432", "postgres", "");
+//Statement statement = c.createStatement();
+//statement.executeUpdate("CREATE DATABASE dibi");
+
+
 get '/users', { req, res -> 
 	res.type("application/json")
 	service.getAllUsers() }
@@ -17,6 +32,18 @@ get '/user/:id', { req, res ->
 	if (u != null) new JsonBuilder([response: u])
 	else {'{"error":"not found" }'
 		}
+}
+
+get '/db/create', { req, res ->
+	res.type("application/json")
+	dbTest.createDatabase()
+	dbTest.createTablePGUserz()
+	dbTest.insertPGUserIntoPGUserz()
+ }
+ 
+get '/db/all', { req, res ->
+	 res.type("text/html")
+	 dbTest.selectAll()
 }
 
 post '/users', { req, res -> 
